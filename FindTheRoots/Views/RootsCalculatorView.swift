5c5baf1731b5f320beefe032da4bd08e52f0ce3f
Jack Dellamea <jdellamea@lcs.on.ca>
2023-01-24 10:03:04 -0500

    Current work on quadratic formula calculator

---
+++ FindTheRoots/Views/RootsCalculatorView.swift
@@ -0,0 +1,36 @@
+//
+//  RootsCalculatorView.swift
+//  FindTheRoots
+//
+//  Created by Jack Dellamea on 2023-01-24.
+//
+
+import SwiftUI
+
+struct RootsCalculatorView: View {
+    
+    //MARK: Stored properties
+    
+    // Input values for co-efficients
+    @State var a: Double = 1
+    @State var b: Double = -6
+    @State var c: Double = 8
+    
+    //List of prior results
+    @State var priorResults: [Result] = []
+    
+    //  MARK: Computed properties
+    
+    // The result, possibilities are:
+    // 1. Discriminant is negative, no real roots
+    // 2. Discriminant is zero, so two equal real roots
+    // 3. Discriminant is positive so two different real roots
+    var result: String {}
+    
+    // The usser interface
+    var body: some View {
+        VStack {}
+            .padding()
+            .navigationTitle("Find the Roots")
+    }
+}
